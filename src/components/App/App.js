b7d58cab17c521e23e872df9ef69942654919357
Erich Engler <erich.engler@hotmail.com>
2023-04-27 18:39:58 -0500

    added routes for search view and favorites view

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,22 @@
+import { HashRouter as Router, Route } from 'react-router-dom';
 import React from 'react';
+import Search from '../Search/Search';
+import Favorites from '../Favorites/Favorites';
 
 function App(props) {
   return (
-    <div>
-      <h1>Giphy Search!</h1>
+    <div className='App'>
+      <Router>
+
+        <Route exact path="/">
+          <Search />
+        </Route>
+
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+
+      </Router>
     </div>
   );
 }
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,9 @@
+function Search () {
+    return (
+        <>
+            <h3>Search</h3>
+        </>
+    )
+}
+
+export default Search;
